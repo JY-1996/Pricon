e0d0ad4dfc6add71d5511c0da2a8630b97073e44
JY <45379044+JY-1996@users.noreply.github.com>
2021-11-03 16:44:18 +0000

    Done list

--- commands/knife/list.js
+++ commands/knife/list.js
@@ -1,85 +1,52 @@
-const { Command } = require("discord-akairo");
 const strings = require("../../lib/string.json");
 const command = require("../../lib/command-info.json");
-const DatabaseManager = require("../../classes/DatabaseManager");
-const { Permissions } = require('discord.js');
-const UtilLib = require("../../api/util-lib");
+const General = require("../../commands/knife/general.js");
 
-class ListCommand extends Command {
-   constructor() {
-      super("list", {
-         aliases: ['l','list'],
-         cooldown: 3000,
-         channel: 'guild',
-         args: [
-            {
-               id: "compensate",
-               match: "flag",
-               flag: ['c','-c','com','-com']
-            },
-            {
-              id: "member",
-              type: "memberMention",
-            }
-         ],
-      });
-   };
+class ListCommand extends General {
+   	constructor() {
+      	super("list", {
+         	aliases: ['l','list'],
+         	cooldown: 3000,
+         	channel: 'guild',
+         	args: [
+            	{
+              		id: "member",
+              		type: "memberMention",
+            	}
+         	],
+      	});
+   	};
 
-   async exec(message, args) {
-      let loadingMsg = await message.channel.send(strings.common.waiting);
+   	async exec(message, args) {
+      	await super.exec(message,args)
 
-      //check channel
-      const db = this.client.db
-      const guildID = message.guild.id
-      const dm = new DatabaseManager(db,guildID)
+		let knifeQuery = await this.dm.getAllKnifeQuery()
+    	let hasKnife = false
+    	let id = ""
+    	if(knifeQuery.empty){
+			this.loadingMsg.edit(command.list.no_reserve.replace('[id]', this.clientID));
+        	return
+		}
 
-      // const knife_channel = await dm.getChannel('knife')
-      // if(!knife_channel){
-      //   loadingMsg.edit(strings.common.no_knife_channel);
-      //   return
-      // }else if(knife_channel != message.channel.id){
-      //   loadingMsg.edit(strings.common.wrong_knife_channel.replace('[channel]', `<#${knife_channel}>`));
-      //   return
-      // }
-
-      let clientID = message.member.id 
-      if(args.member){
-        if(message.member.hasPermission(Permissions.FLAGS.ADMINISTRATOR)){
-          clientID = args.member.id
-        }else{
-          loadingMsg.edit(strings.common.no_permission);
-          return
-        }
-      }
-
-      const member = await message.guild.members.fetch(clientID)
-      const clientName = UtilLib.extractInGameName(member.displayName, false)
-      await dm.setClientID(clientID)
-
-      let knifeQuery = await dm.getAllKnifeQuery()
-      if (knifeQuery.empty) {
-        loadingMsg.edit(command.list.no_reserve.replace('[id]',clientID));
-        return
-      }
-      let text = command.list.title.replace('[id]',clientID)
-      await knifeQuery.forEach(doc => {
-            let data = doc.data()
-            if(data.kill){
-               let kill_detail = UtilLib.convertKillToPhaseWeekBoss(data.kill)
-               text += "\t已出刀" + kill_detail.phase + "階" + kill_detail.week + "周" + kill_detail.boss + "王"
+		let text = ""
+      	knifeQuery.forEach(doc => {
+			let data = doc.data()
+			text += data.boss + "王"
+            if(data.status == "done"){
+               	text += "\t已出刀"
             }else{
-               text += "\t预约" + data.boss + "王中。。。"
+               	text += "\t预约中。。。"
             }
 
             if(data.compensate){
-               text += "(0.5刀)\n"
+               	text += "(0.5刀)\n"
             }else{
-               text += "(1刀)\n"
+               	text += "(1刀)\n"
             }
-         }
-      )
-      loadingMsg.edit(text);
-      return
+      	})
+		
+      	this.loadingMsg.edit(text);
+      	return
    };
 
 }
